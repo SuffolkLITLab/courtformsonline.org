9663f8f3c0e82f5e1f0339bf8f6ecbf49eb8d715
jtmst <josh.troy.mills@gmail.com>
2024-04-17 16:03:29 -0400

    added deploy.yaml file, set up next for static build output

--- courtformsonline.org/next.config.js
+++ courtformsonline.org/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/courtformsonline.org' : '',
+  assetPrefix: isProd ? '/courtformsonline.org' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig;
