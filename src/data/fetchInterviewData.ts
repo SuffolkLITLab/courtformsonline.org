a69181cc03e01b948ea98a4382924c771b0d518d
jtmst <josh.troy.mills@gmail.com>
2024-05-08 15:38:42 -0400

    prettier

--- src/data/fetchInterviewData.ts
+++ src/data/fetchInterviewData.ts
@@ -1,7 +1,6 @@
-
 import { formSources } from '../config/formSources.config';
 import { legalTopics } from '../config/topics.config';
-import { findClosestTopic } from './helpers'
+import { findClosestTopic } from './helpers';
 
 export const fetchInterviews = async () => {
   const serverUrl = formSources.docassembleServers[0].url;
@@ -14,27 +13,29 @@ export const fetchInterviews = async () => {
     const interviewsByTopic = {};
     const titlesInTopics = {};
 
-    legalTopics.forEach(topic => {
+    legalTopics.forEach((topic) => {
       interviewsByTopic[topic.name] = [];
-      titlesInTopics[topic.name] = new Set(); 
+      titlesInTopics[topic.name] = new Set();
     });
 
     if (data && data.interviews) {
-      data.interviews.forEach(interview => {
+      data.interviews.forEach((interview) => {
         const uniqueTopics = new Set();
 
         // match topics to config by metadata.LIST_Topic, and tags values
-        (interview.metadata.LIST_topics || []).concat(interview.tags || []).forEach(code => {
-          const topic = findClosestTopic(code, legalTopics);
-          if (topic && !uniqueTopics.has(topic.name)) {
-            uniqueTopics.add(topic.name);
-            // check if the title already exists in the topic to avoid duplicated titles
-            if (!titlesInTopics[topic.name].has(interview.title)) {
-              interviewsByTopic[topic.name].push(interview);
-              titlesInTopics[topic.name].add(interview.title);
+        (interview.metadata.LIST_topics || [])
+          .concat(interview.tags || [])
+          .forEach((code) => {
+            const topic = findClosestTopic(code, legalTopics);
+            if (topic && !uniqueTopics.has(topic.name)) {
+              uniqueTopics.add(topic.name);
+              // check if the title already exists in the topic to avoid duplicated titles
+              if (!titlesInTopics[topic.name].has(interview.title)) {
+                interviewsByTopic[topic.name].push(interview);
+                titlesInTopics[topic.name].add(interview.title);
+              }
             }
-          }
-        });
+          });
 
         // add to other if no matching topic in config
         if (uniqueTopics.size === 0) {
