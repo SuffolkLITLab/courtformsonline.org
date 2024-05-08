985a57c80a6478bf7b92ba0ff20f0bdd8cf14876
jtmst <josh.troy.mills@gmail.com>
2024-05-08 16:56:18 -0400

    conflicts

---
+++ __tests__/index.test.js
@@ -0,0 +1,40 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import TopicsPage from '../src/app/page';
+import RootLayout from '../src/app/layout'
+
+describe('Layout component', () => {
+  beforeEach(() => {
+    render(<RootLayout />);
+  });
+
+  it('should contain a navigation bar', () => {
+    const navElement = screen.getByRole('navigation');
+    expect(navElement).toBeInTheDocument();
+  });
+
+  it('should contain a footer', () => {
+    const footerElement = screen.getByRole('contentinfo');
+    expect(footerElement).toBeInTheDocument();
+  });
+});
+
+describe('TopicsPage', () => {
+  beforeEach(() => {
+    render(<TopicsPage />);
+  });
+  it('renders the hero section', () => {
+    const heroSection = document.getElementById('hero-section');
+    expect(heroSection).toBeInTheDocument();
+  });
+
+  it('renders the how it works section', () => {
+    const howItWorksSection = document.getElementById('how-it-works-section');
+    expect(howItWorksSection).toBeInTheDocument();
+  });
+
+  it('renders the topics section', () => {
+    const topicsSection = document.getElementById('topics');
+    expect(topicsSection).toBeInTheDocument();
+  });
+});
