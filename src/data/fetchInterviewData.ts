eaffb0cf447b4bb25d5a9540f11250ff2548460d
jtmst <josh.troy.mills@gmail.com>
2024-05-08 15:33:10 -0400

    refactored topic sorting

--- src/data/fetchInterviewData.ts
+++ src/data/fetchInterviewData.ts
@@ -1,5 +1,7 @@
+
 import { formSources } from '../config/formSources.config';
 import { legalTopics } from '../config/topics.config';
+import { findClosestTopic } from './helpers'
 
 export const fetchInterviews = async () => {
   const serverUrl = formSources.docassembleServers[0].url;
@@ -9,51 +11,45 @@ export const fetchInterviews = async () => {
   try {
     const response = await fetch(url.toString());
     const data = await response.json();
+    const interviewsByTopic = {};
+    const titlesInTopics = {};
 
-    const interviewsByTopic = { Other: [] };
+    legalTopics.forEach(topic => {
+      interviewsByTopic[topic.name] = [];
+      titlesInTopics[topic.name] = new Set(); 
+    });
 
     if (data && data.interviews) {
-      data.interviews.forEach((interview) => {
-        let assigned = false;
+      data.interviews.forEach(interview => {
+        const uniqueTopics = new Set();
 
-        // Prioritize 'LIST_topics' if it exists
-        const listTopics = interview.metadata?.LIST_topics || [];
-        if (listTopics.length > 0) {
-          listTopics.forEach((topicCode) => {
-            const topic = legalTopics.find((t) => t.codes.includes(topicCode));
-            if (topic) {
-              if (!interviewsByTopic[topic.name]) {
-                interviewsByTopic[topic.name] = [];
-              }
+        // match topics to config by metadata.LIST_Topic, and tags values
+        (interview.metadata.LIST_topics || []).concat(interview.tags || []).forEach(code => {
+          const topic = findClosestTopic(code, legalTopics);
+          if (topic && !uniqueTopics.has(topic.name)) {
+            uniqueTopics.add(topic.name);
+            // check if the title already exists in the topic to avoid duplicated titles
+            if (!titlesInTopics[topic.name].has(interview.title)) {
               interviewsByTopic[topic.name].push(interview);
-              assigned = true;
+              titlesInTopics[topic.name].add(interview.title);
             }
-          });
-        }
+          }
+        });
 
-        // Use 'tags' only if LIST_topics doesnt exist or doesnt match any topics
-        if (!assigned) {
-          const tags = interview.tags || [];
-          tags.forEach((tag) => {
-            const topic = legalTopics.find((t) => t.codes.includes(tag));
-            if (topic) {
-              if (!interviewsByTopic[topic.name]) {
-                interviewsByTopic[topic.name] = [];
-              }
-              interviewsByTopic[topic.name].push(interview);
-              assigned = true;
-            }
-          });
-        }
-
-        // If no topics were assigned, place in Other
-        if (!assigned) {
-          interviewsByTopic['Other'].push(interview);
+        // add to other if no matching topic in config
+        if (uniqueTopics.size === 0) {
+          interviewsByTopic['Other'] = interviewsByTopic['Other'] || [];
+          if (!titlesInTopics['Other'].has(interview.title)) {
+            interviewsByTopic['Other'].push(interview);
+            titlesInTopics['Other'].add(interview.title);
+          }
         }
       });
     }
+
     return { interviewsByTopic, isError: false };
   } catch (error) {
+    console.error('Failed to fetch interviews:', error);
     return { interviewsByTopic: {}, isError: true };
   }
 };
