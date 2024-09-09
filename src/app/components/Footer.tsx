cbe60f1dbd79b6b4c76cb3d8e683ab38507de5fd
samglover <sam@samglover.net>
2024-09-09 13:56:24 -0500

    Move footer styles to a CSS module; fix footer logo responsiveness

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,18 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { prefix } from '../../../prefix';
+import styles from '../css/Footer.module.css';
 
 export default function Footer() {
   return (
-    <footer role="contentinfo" className="text-center py-4">
+    <footer
+      id="footer"
+      role="contentinfo"
+      className={styles.Footer + ' text-center py-4'}
+    >
       <div className="container">
         <div className="row">
           <div className="col">
-            <a href="https://suffolklitlab.org">
+            <a href="https://suffolklitlab.org" className={styles.SuffolkLogo}>
               <Image
                 src="lit-lab-logo-large.svg"
                 alt="Suffolk University Law School Legal Information & Technology Lab logo"
-                className="img-fluid mb-3 suffolk-logo"
+                className="img-fluid mb-3"
                 width={792}
                 height={321.38}
               />
