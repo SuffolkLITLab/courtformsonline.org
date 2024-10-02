5eb14ccf79b594de8f75076334ec4e19c9e675ee
Sam Glover <sam.glover@suffolk.edu>
2024-10-02 11:13:58 -0500

    Search Console redirects (#100)
    * Add regex redirect for .pdf urls
    
    * Redirect Slack archive URLs to the documentation website
    
    * Add non-regex redirects for specific URLs
    
    * Update Form Explorer redirects; remove others (for now)
    
    * Remove unnecessary comments

--- next.config.js
+++ next.config.js
@@ -1,9 +1,24 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
+  async redirects() {
+    return [
+      // Form Explorer PDFs
+      {
+        source: '/forms/:slug([a-zA-Z\\d]*.pdf)',
+        destination: 'https://formexplorer.suffolklitlab.org/forms/:slug',
+        basePath: false,
+        permanent: true,
+      },
+      // Form Explorer CSV
+      {
+        source: '/forms/form_data.csv',
+        destination:
+          'https://formexplorer.suffolklitlab.org/forms/form_data.csv',
+        basePath: false,
+        permanent: true,
+      },
+    ];
   },
 };
 
