6ed2edb61abe5d919a49a3fd54de69f9e85230a6
Sam Glover <sam.glover@suffolk.edu>
2024-10-03 09:21:34 -0500

    Remove redirects (#104)

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  async redirects() {
-    return [
-      // Form Explorer PDFs
-      {
-        source: '/forms/:slug([a-zA-Z\\d]*.pdf)',
-        destination: 'https://formexplorer.suffolklitlab.org/forms/:slug',
-        basePath: false,
-        permanent: true,
-      },
-      // Form Explorer CSV
-      {
-        source: '/forms/form_data.csv',
-        destination:
-          'https://formexplorer.suffolklitlab.org/forms/form_data.csv',
-        basePath: false,
-        permanent: true,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
