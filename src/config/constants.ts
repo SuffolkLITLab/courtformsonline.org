923e759898063f282091553a6b54fb6551ee0635
Quinten Steenhuis <qsteenhuis@gmail.com>
2025-12-01 20:20:43 -0500

    test pre-commit hook (should run Prettier)

---
+++ src/config/constants.ts
@@ -0,0 +1,6 @@
+// Application-wide constants
+
+// Maximum number of interviews to show in an expanded card
+export const MAX_VISIBLE_PER_CARD = 10;
+// Number of categories visible by default (e.g., with 3 columns, 9 items = 3 rows)
+export const MAX_VISIBLE_CATEGORIES = 12;
