53d71c9f31098de490502cfa18049cc1bd61caf6
Quinten Steenhuis <qsteenhuis@suffolk.edu>
2026-07-07 14:39:15 -0400

    Show up to 18 category cards

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,7 +3,7 @@
 // Maximum number of interviews to show in an expanded card
 export const MAX_VISIBLE_PER_CARD = 10;
 // Number of categories visible by default (e.g., with 3 columns, 9 items = 3 rows)
-export const MAX_VISIBLE_CATEGORIES = 12;
+export const MAX_VISIBLE_CATEGORIES = 18;
 // Maximum number of related forms to show on form landing page
 export const MAX_RELATED_FORMS = 5;
 
